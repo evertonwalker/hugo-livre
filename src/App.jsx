0fb0d7675c6e02c1a00e37dc69b29777d2358cc0
Everton Walker <everton.walker@hotmail.com>
2026-09-10 11:02:54 -0300

    feat: change marging

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Comments from './components/Comments'
-import Countdown from './components/Countdown'
+import Comments from "./components/Comments";
+import Countdown from "./components/Countdown";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-black text-white antialiased">
       <main className="w-full px-4 sm:px-8">
-        <div className="mx-auto flex min-h-[85vh] max-w-7xl flex-col justify-center py-16">
+        <div className="mx-auto flex min-h-[85vh] max-w-7xl flex-col justify-center py-8">
           <Countdown />
         </div>
 
@@ -14,5 +14,5 @@ export default function App() {
         </div>
       </main>
     </div>
-  )
+  );
 }
